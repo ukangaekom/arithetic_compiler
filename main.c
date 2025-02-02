fc4077642cc8020de90e1b8a124e673de7bcb43e
ukangaekom <ekomabasiuk@gmail.com>
2025-02-02 07:25:43 +0100

    arithmetic compiler

---
+++ main.c
@@ -0,0 +1,11 @@
+//  A SIMPLE COMPILER IN C
+
+// Importing libraries
+#include <stdio.h>; //standard IO library
+
+#include <stdlib.h>; //standard library function - memory allocation
+
+#include <ctype.h>; //Isidigit()
+
+#include <string.h>; //String Manipulation
+
